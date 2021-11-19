2441f0a816f8941c0f41d406049dd689c4fa4f4a
Thornebanks <akshays.singh613@gmail.com>
2021-11-19 13:35:15 -0500

    map done

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,12 @@
 import "./styles/partials/_typography.scss";
 import Nav from "./components/Nav";
+import Map from "./components/map/map"
 import "./App.scss";
 
 function App() {
   return (
     <>
+      <Map />
       <Nav />
     </>
   );
