7fefb4af9034187a05eeec679b47321aa8f687dc
Thornebanks <akshays.singh613@gmail.com>
2021-11-19 15:42:10 -0500

    final commit

--- app/src/App.js
+++ app/src/App.js
@@ -1,12 +1,14 @@
 import "./styles/partials/_typography.scss";
 import Nav from "./components/Nav";
 import Map from "./components/map/map"
+import Prototype from "./components/prototype/prototype";
 import "./App.scss";
 
 function App() {
   return (
     <>
       <Map />
+      <Prototype />
       <Nav />
     </>
   );
--- app/src/components/prototype/prototype.jsx
+++ app/src/components/prototype/prototype.jsx
@@ -1,18 +1,19 @@
 import React from "react";
 import "./prototype.scss";
 import home from "../../assets/logos/home.svg";
+import home2 from "../../assets/logos/home2.svg"
 import ellipse from "../../assets/logos/Ellipse 7.svg";
 import work from "../../assets/logos/work.svg";
 import add1 from "../../assets/logos/add1.svg";
 import add2 from "../../assets/logos/add2.svg";
 
 function Prototype() {
-  reutrn(
+  return(
     <>
       <section className="prototype">
         <h1 className="prototype__title">Hello, Dan</h1>
       </section>
-      <form>
+      <form className="prototype__form">
         <input
           className="prototype__search"
           type="text"
@@ -22,34 +23,37 @@ function Prototype() {
         />
       </form>
       <section className="prototype__section">
-        <h3>Favorities</h3>
-        <h3>See All</h3>
+        <h3 className="prototype__section prototype__section--fav">Favorities</h3>
+        <h3 className="prototype__section prototype__section--see">See All</h3>
       </section>
-      <div>
-        <img src={home} alt="home icon" />
-        <img src={ellipse} alt="background" />
-        <h6>Home</h6>
+      <div className="flex">
+          <div className="prototype__icon">
+            <img className="prototype__icon prototype__icon--home" src={home} alt="home icon" />
+            <img className="prototype__icon prototype__icon--home2" src={home2} alt="top part" />
+            <img className="prototype__ellipse prototype__ellipse--bg" src={ellipse} alt="background" />
+            <h6>Home</h6>
+          </div>
+          <div className="prototype__icon">
+            <img className="prototype__icon prototype__icon--home" src={work} alt="work icon" />
+            <img className="prototype__ellipse prototype__ellipse--bg1" src={ellipse} alt="background" />
+            <h6>Work</h6>
+          </div>
+          <div className="prototype__icon">
+            <div>
+              <img className="prototype__ellipse prototype__ellipse--bg2" src={ellipse} alt="background" />
+              <img
+                className="prototype__icon prototype__icon--add"
+                src={add1}
+                alt="add icon"
+              />
+              <img
+                className="prototype__icon prototype__icon--add2"
+                src={add2}
+                alt="add icon"
+              />
+              <h6>Add</h6>
+            </div>
       </div>
-      <div>
-        <img src={work} alt="work icon" />
-        <img src={ellipse} alt="background" />
-        <h6>Work</h6>
-      </div>
-      <div className="prototype__icon">
-        <div>
-          <img src={ellipse} alt="background" />
-          <img
-            className="prototype__icon prototype__icon--horizontal"
-            src={add1}
-            alt="add icon"
-          />
-          <img
-            className="prototype__icon prototype__icon--vertical"
-            src={add2}
-            alt="add icon"
-          />
-          <h6>Add</h6>
-        </div>
       </div>
     </>
   );
