7c77c164a8b28a1e08665637a291dd769d36d1ba
Thornebanks <akshays.singh613@gmail.com>
2021-11-19 14:39:23 -0500

    dot file

--- app/src/components/map/map.jsx
+++ app/src/components/map/map.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import map from "../../assets/logos/Frame 8.svg"
+import person from "../../assets/logos/dot.svg"
 import './map.scss';
 
 function Map(){
     return(
-        <>
-        <img className="map" src={map} alt="view of the map" />
-        </>
+        <div className="map">
+        <img className="map__area" src={map} alt="view of the map" />
+        <img className="map__person" src={person} alt="" />
+        </div>
     );
 }
 
